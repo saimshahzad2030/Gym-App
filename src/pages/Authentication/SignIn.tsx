dd32dbd2cd1a0afb0cd4f615180dbdc4b62384b9
Saim Shahzad <saimshehzad2030@gmail.com>
2024-11-07 16:40:03 +0500

    few changes

--- src/pages/Authentication/SignIn.tsx
+++ src/pages/Authentication/SignIn.tsx
@@ -155,7 +155,7 @@ const SignIn: React.FC = () => {
             </div>
           </div>
 
-          <div className="w-full border-stroke dark:border-strokedark xl:w-1/2 xl:border-l-2">
+          <div className="mt-12 sm:mt-0 w-full border-stroke dark:border-strokedark xl:w-1/2 xl:border-l-2">
             <div className="w-full p-4 sm:p-12.5 xl:p-17.5"> 
               <h2 className="mb-9 text-2xl font-bold text-black dark:text-white sm:text-title-xl2">
                 Sign In to FFG
