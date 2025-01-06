ccd754e1cd76ad0b2b1838eb875a2cbf812adb78
Saim Shahzad <saimshehzad2030@gmail.com>
2025-01-06 10:55:49 +0500

    changed url

--- src/config/config.tsx
+++ src/config/config.tsx
@@ -1,6 +1,4 @@
 export const config = {
-    // url:'https://3.111.221.228/'
-    // url:'https://5f2e-3-111-221-228.ngrok-free.app/'
-url:'https://0nn4jvzhwj.execute-api.ap-south-1.amazonaws.com/'
-    // url:'http://3.111.221.228:8080/'
-}
+  // url:'https://0nn4jvzhwj.execute-api.ap-south-1.amazonaws.com/'
+  url: 'http://localhost:8000/',
+};
